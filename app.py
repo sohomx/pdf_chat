1cab00a4b70b374af3129bfd6747844892463b14
Sohom Pal <sohom377@gmail.com>
2023-08-04 09:30:51 +0530

    extract text from the pdfs

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 import streamlit as st
-from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_documents):
+    text = ""
+    for pdf in pdf_documents:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Chat with Multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title='Chat Multiple PDFs', page_icon=':books:')
 
-    st.header("Chat with multiple PDFs :books:")
-    st.text_input("Ask a question about your documents:")
+    if 'text' not in st.session_state:
+        st.session_state.text = ""
 
     with st.sidebar:
-        st.subheader("Your Documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.button("Processing"):
+        pdf_documents = st.file_uploader('Upload your PDF file', type=[
+                                         'pdf'], accept_multiple_files=True)
+
+        if st.button('Process'):
+            with st.spinner('Processing...'):
+                pdf_text = get_pdf_text(pdf_documents)
+
+                st.session_state.text = pdf_text
+
+                # text_chunks = get_chunks(pdf_text)
+
+                st.success('Done!')
+
+    st.title('Chat with Multiple PDFs :books:')
+    st.text_input('Ask a question about the PDFs')
+
+    st.write(st.session_state.text)
 
 
 if __name__ == '__main__':
     main()
-
 # Note
 # run the following streamlite command with: python3 -m streamlit run app.py
