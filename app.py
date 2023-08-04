11c7495d3e7f7733be0949abe44e81dc1b6b3812
Sohom Pal <sohom377@gmail.com>
2023-08-04 08:54:37 +0530

    add ur api keys

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from dotenv import load_dotenv
+
 
 def main():
-    st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
+    load_dotenv()
+    st.set_page_config(page_title="Chat with Multiple PDFs",
+                       page_icon=":books:")
 
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
@@ -10,6 +14,7 @@ def main():
         st.subheader("Your Documents")
         st.file_uploader("Upload your PDFs here and click on 'Process'")
         st.button("Process")
+
 
 if __name__ == '__main__':
     main()
