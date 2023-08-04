921b5cf4283f00af77821ceee87ffd15f684e5db
Sohom Pal <sohom377@gmail.com>
2023-08-04 08:39:19 +0530

    create GUI

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your Documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
+
+# Note
+# run the following streamlite command with: python3 -m streamlit run app.py
